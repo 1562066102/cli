fa23e808aca8136051ed72aecaa04822b8956087
hyh <1562066102@qq.com>
2022-03-24 10:27:35 +0800

    feat: ✨ 替换使用配置合并发发

--- package/hyh-scripts/src/start.js
+++ package/hyh-scripts/src/start.js
@@ -2,12 +2,16 @@
 
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const webpackConfig = require('../config/webpack.config');
+const mergeWebpackConfig = require('../tool/merge');
+
+const nodeEnv = 'development';
 
 function start() {
+  process.env.NODE_ENV = nodeEnv;
   console.log('starting server...');
-  const compiler = Webpack(webpackConfig);
-  const server = new WebpackDevServer({...webpackConfig.devServer}, compiler);
+  const config = mergeWebpackConfig(nodeEnv);
+  const compiler = Webpack(config);
+  const server = new WebpackDevServer({...config.devServer}, compiler);
   server.startCallback(() => {
     const localIPv4 = WebpackDevServer.internalIPSync('v4');
     console.log('Successfully started server on http://localhost:8080');
