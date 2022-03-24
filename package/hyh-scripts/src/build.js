45217a45553f2ea3cd50cfa4783dffeed703468f
hyh <1562066102@qq.com>
2022-03-24 10:26:42 +0800

    feat: ✨ 初步完成构建命令

--- package/hyh-scripts/src/build.js
+++ package/hyh-scripts/src/build.js
@@ -1,7 +1,20 @@
 'use strict';
 
-function build(e) {
-  console.log('build', e);
+const Webpack = require('webpack');
+const mergeWebpackConfig = require('../tool/merge');
+
+const nodeEnv = 'production';
+
+function build() {
+  process.env.NODE_ENV = nodeEnv;
+  console.log('building server...');
+  const config = mergeWebpackConfig(nodeEnv);
+  Webpack(config, (err, stats) => {
+    if (err || stats.hasErrors()) {
+      console.log(err);
+    }
+    console.log('处理完成');
+  });
 }
 
 module.exports = build;
