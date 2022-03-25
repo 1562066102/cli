1667548e7ace18a2570f42b1d956cda889a44181
hyh <1562066102@qq.com>
2022-03-25 16:50:56 +0800

    feat: ✨ 路径统一管理

---
+++ package/hyh-scripts/tool/paths.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const appRootDir = process.cwd();
+const analyseAppPath = relativePath => path.resolve(appRootDir, relativePath);
+const analyseUnknownExtensionPath = relativePath => {
+  const extensions = ['mjs', 'js', 'ts', 'jsx', 'tsx', 'json'];
+  const extension = extensions.find(extension =>
+    fs.existsSync(`${relativePath}.${extension}`)
+  );
+  return analyseAppPath(`${relativePath}.${extension || 'js'}`);
+};
+
+const paths = {
+  env: analyseAppPath('env'),
+  entry: analyseUnknownExtensionPath('src/index'),
+  output: analyseAppPath('dist'),
+  src: analyseAppPath('src'),
+  public: analyseAppPath('public'),
+  html: analyseAppPath('public/index.html'),
+};
+
+module.exports = paths;
