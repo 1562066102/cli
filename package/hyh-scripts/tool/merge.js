24a8fbfa5bf20d954fb3a73dc613e82646982d27
hyh <1562066102@qq.com>
2022-04-25 16:00:16 +0800

    refactor: 🦄 修改为函数形式、入参、自定义配置获取方式

--- package/hyh-scripts/tool/merge.js
+++ package/hyh-scripts/tool/merge.js
@@ -7,22 +7,21 @@ const webpackDevConfig = require('../config/webpack.config.dev');
 const webpackProdConfig = require('../config/webpack.config.prod');
 
 /** 合并webpack配置 */
-function mergeWebpackConfig(mode = 'development') {
+const mergeWebpackConfig = () => {
+  const mode = process.env.NODE_ENV;
   let customWebpackConfig = {};
   try {
-    const customWebpackConfigPath = path.resolve(
-      process.cwd(),
-      './hyh.config.js'
-    );
+    const customWebpackConfigPath = path.resolve('./hyh.config.js');
     customWebpackConfig = require(customWebpackConfigPath);
   } catch (error) {
     // 无自定义配置文件
   }
+
   return merge(
     webpackConfig,
     mode === 'development' ? webpackDevConfig : webpackProdConfig,
     customWebpackConfig
   );
-}
+};
 
 module.exports = mergeWebpackConfig;
